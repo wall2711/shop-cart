702860424138455eb61ae45a0dcead82bb143301
wall2711 <markvenzon975@gmail.com>
2022-11-25 19:07:43 +0800

    dataset done

---
+++ dataset.php
@@ -0,0 +1,88 @@
+<?php
+    $arrProducts = array(
+        array(
+            'name'          => "oversized t-shirt in khaki-Green",
+            'description'   => "Renmark Pogi",
+            'price'         => "399",
+            'photo1'        => "itemA1.png",
+            'photo2'        => "itemB1.png"
+        ),
+        array(
+            'name'          => "Fila T Shirt Bellano (men)",
+            'description'   => "Renmark Pogi",
+            'price'         => "499",
+            'photo1'        => "itemA2.png",
+            'photo2'        => "itemB2.png",
+        ),
+        array(
+            'name'          => "T-Shirt Junior - Orange/O'TRNCE - Kids",
+            'description'   => "Renmark Pogi",
+            'price'         => "299",
+            'photo1'        => "itemA3.png",
+            'photo2'        => "itemB3.png",
+        ),
+        array(
+            'name'          => "Mons Royale Redwood Enduro VT Women's Jersey 2022",
+            'description'   => "Renmark Pogi",
+            'price'         => "399",
+            'photo1'        => "itemA4.png",
+            'photo2'        => "itemB4.png",
+        ),
+        array(
+            'name'          => "MANGO - Oversized striped sweater Black - S - Woman",
+            'description'   => "Renmark Pogi",
+            'price'         => "999",
+            'photo1'        => "itemA5.png",
+            'photo2'        => "itemB5.png",
+        ),
+        array(
+            'name'          => "Montura Outdoor Colorblock Short Sleeve T-shirt Grey XL Man",
+            'description'   => "Renmark Pogi",
+            'price'         => "599",
+            'photo1'        => "itemA6.png",
+            'photo2'        => "itemB6.png",
+        ),
+        array(
+            'name'          => "Ballin 23 Tee taxi yellow - Mister Tee MT1408 - Size M",
+            'description'   => "Renmark Pogi",
+            'price'         => "799",
+            'photo1'        => "itemA7.png",
+            'photo2'        => "itemB7.png",
+        ), 
+        array(
+            'name'          => "Adidas Originals 3 Stripe T Shirt Black",
+            'description'   => "Renmark Pogi",
+            'price'         => "399",
+            'photo1'        => "itemA8.png",
+            'photo2'        => "itemB8.png",
+        ),
+        array(
+            'name'          => "Mens Asymmetric Colorblock Letter Printed Short Sleeve T-Shirts",
+            'description'   => "Renmark Pogi",
+            'price'         => "999",
+            'photo1'        => "itemA9.png",
+            'photo2'        => "itemB9.png",
+        ),
+        array(
+            'name'          => "Nike everyday Modern boxy crop t-shirt in cinnabar brown",
+            'description'   => "Renmark Pogi",
+            'price'         => "399",
+            'photo1'        => "itemA10.png",
+            'photo2'        => "itemB10.png",
+        ),
+        array(
+            'name'          => "Morvelo Women's Definitive Baselayer - M - White",
+            'description'   => "Renmark Pogi",
+            'price'         => "599",
+            'photo1'        => "itemA11.png",
+            'photo2'        => "itemB11.png",
+        ),
+        array(
+            'name'          => "Morvelo Women's Definitive Baselayer - M - White",
+            'description'   => "Renmark Pogi",
+            'price'         => "799",
+            'photo1'        => "itemA12.png",
+            'photo2'        => "itemB12.png",
+        )
+    );
+?>

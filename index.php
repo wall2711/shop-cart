efd2b8390f71b6adeea6e2ba3a97aa27a3a59d12
wall2711 <markvenzon975@gmail.com>
2022-11-25 20:14:27 +0800

    done edit codes

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/styles.css">
-    <title>Kickz Shop Online Shop | Shopping Cart</title>
+    <title>Online clothing Shop</title>
 </head>
 <body>
     <div class="container">
         <div class="row mt-5">
             <div class="col-10">
-                <h1><i class="fa fa-store"></i> Kickz Shop</h1>
+                <h1><i class="fa fa-store"></i> Online clothing Shop</h1>
             </div>
             <div class="col-2 text-right">
                 <a href="cart.php" class="btn btn-primary">
